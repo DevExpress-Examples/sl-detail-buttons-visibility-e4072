a2285d896489d0c84915d20dcfcd3f53db85d165
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SLShowDetailButton/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Globalization;
+using WpfApplication26;
+
+namespace SLShowDetailButton {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+            grid1.ItemsSource = new DataSource().Categories;
+        }
+    }
+
+    public class MyConverter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            // Obtaining the value to be converted 
+            string categoryValue = (string)value;
+
+            // Specifying values for which to show expand buttons
+            string[] categories = new string[] { "First", "Third" };
+            if (categories.Contains(categoryValue))
+                return true;
+
+            // Disable expand button if the value isn't in the list
+            return false;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            return null;
+        }
+    }
+}
---
+++ VB/SLShowDetailButton/MainPage.xaml.vb
@@ -0,0 +1,37 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Globalization
+Imports WpfApplication26
+
+Namespace SLShowDetailButton
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			grid1.ItemsSource = New DataSource().Categories
+		End Sub
+	End Class
+
+	Public Class MyConverter
+		Implements IValueConverter
+		Public Function Convert(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.Convert
+			' Obtaining the value to be converted 
+			Dim categoryValue As String = CStr(value)
+
+			' Specifying values for which to show expand buttons
+			Dim categories() As String = { "First", "Third" }
+			If categories.Contains(categoryValue) Then
+				Return True
+			End If
+
+			' Disable expand button if the value isn't in the list
+			Return False
+		End Function
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As CultureInfo) As Object Implements IValueConverter.ConvertBack
+			Return Nothing
+		End Function
+	End Class
+End Namespace
